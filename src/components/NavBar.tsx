52e9c7ac2b068698646f435ca1b7fbecd8ea0880
umfb <umfbankltd1@gmail.com>
2025-02-27 11:24:32 +0100

    added .avif and .webp image format

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,8 +3,8 @@ export default function NavBar() {
     <header>
       <nav className="flex justify-center px-3 lg:px-0">
         <picture>
-          <source srcset="/logo.avif" type="image/avif"/>
-          <source srcset="/logo.webp" type="image/webp"/>
+          <source srcSet="/logo.avif" type="image/avif"/>
+          <source srcSet="/logo.webp" type="image/webp"/>
           <img
           src="/logo.png"
           style={{ width: "100%", maxWidth: "450px" }}
