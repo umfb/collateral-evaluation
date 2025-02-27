83fb97d94ab64749351eaaaec1fad9b164a04441
umfb <umfbankltd1@gmail.com>
2025-02-27 11:22:28 +0100

    linked the labels to their respective input/select tags

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,11 +2,16 @@ export default function NavBar() {
   return (
     <header>
       <nav className="flex justify-center px-3 lg:px-0">
-        <img
+        <picture>
+          <source srcset="/logo.avif" type="image/avif"/>
+          <source srcset="/logo.webp" type="image/webp"/>
+          <img
           src="/logo.png"
           style={{ width: "100%", maxWidth: "450px" }}
           alt="unilagmfb-logo"
         />
+        </picture>
+        
       </nav>
     </header>
   );
